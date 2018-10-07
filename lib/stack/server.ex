29bc7c3e74214114670370c1b9059e73697c433f
Yifeng Wang <yifeng@yeefom.com>
2018-10-06 17:26:27 -0700

    add server

---
+++ lib/stack/server.ex
@@ -0,0 +1,12 @@
+defmodule Stack.Server do
+  use GenServer
+
+  def init(stack \\ []) do
+    { :ok, stack }
+  end
+
+  def handle_call(:pop, _from, stack) do
+    { popped, new_stack } = List.pop_at(stack, -1)
+    { :reply, popped, new_stack}
+  end
+end
